8e8b833b6399cea191d52eea85209229a581781b
Sajad Sharhani <sharhani.sajad@gmail.com>
2022-02-03 03:05:22 +0330

    adding color

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 #!/usr/bin/env node
 const pkgJSON = require("./package.json");
 const welcome = require("cli-welcome");
+const chalk = require("chalk");
+const log = console.log;
 
 welcome({
-  title: pkgJSON.name,
+  title: `Sajad Sharhani`,
   tagLine: `Get to know Sajad`,
   description: pkgJSON.description,
   version: pkgJSON.version,
-  bgColor: `#FADC00`,
+  bgColor: `#6937FF`,
   color: `#000000`,
   bold: true,
   clear: true,
 });
 
-console.log(`
-    
-Sajad Sharhani - Software Engineer
+log(
+  `${chalk.italic.bold(
+    `Sajad is a CLI tool that helps you to get to know Sajad.`
+  )}
 
-Sajad is a CLI tool that helps you to get to know Sajad.`);
+${chalk.hex(`6cc644`).bold.inverse(` GitHub `)} ${chalk.dim(
+    `https://github.com/Sajad-Sharhani`
+  )}
+${chalk.hex(`1da1f2`).bold.inverse(` Twitter `)} ${chalk.dim(
+    `https://twitter.com/SajadSharhani`
+  )}
+${chalk.hex(`0077b5`).bold.inverse(` Linkedin `)} ${chalk.dim(
+    `https://www.linkedin.com/in/sajad-sharhani-757509137/`
+  )}
+`
+);
