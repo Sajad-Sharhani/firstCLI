d40c59ed3f7e8002d88ad93fdd9642edaf2e907d
Sajad Sharhani <sharhani.sajad@gmail.com>
2022-02-03 04:22:26 +0330

    adding symbols

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 const pkgJSON = require("./package.json");
 const welcome = require("cli-welcome");
 const chalk = require("chalk");
 const log = console.log;
+
+import sym from "log-symbols";
+// const sym = require("log-symbols");
+const success = chalk.green;
+const error = chalk.red.bold;
+const warning = chalk.keyword(`orange`);
+const info = chalk.blue;
 
 welcome({
   title: `Sajad Sharhani`,
@@ -31,3 +40,14 @@ ${chalk.hex(`0077b5`).bold.inverse(` Linkedin `)} ${chalk.dim(
   )}
 `
 );
+
+log(`
+${sym.success} ${success(` SUCCESS `)} Thanks for ckecking out my CLI.
+${sym.info} ${info(
+  ` INFO `
+)} You can find more information about me at my GitHub profile.
+${sym.warning} ${warning(
+  ` WARNING `
+)} You can find more information about me at my Twitter profile.
+${sym.error} ${error(` ERROR `)} I hope you enjoy it.
+`);
