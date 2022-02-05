b5facff3f87ba3a694dbc6a359e39903661fa269
Sajad Sharhani <sharhani.sajad@gmail.com>
2022-02-05 04:09:53 +0330

    spinner

--- utils/stats.js
+++ utils/stats.js
@@ -2,14 +2,18 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const handleError = require("cli-handle-error");
 import fetch from "node-fetch";
+import ora from "ora";
 
+const spinner = ora({ text: "Loading...", color: "yellow" });
 const apiURL = `https://api.github.com/users/Sajad-Sharhani`;
 export default async () => {
   try {
+    spinner.start(`Fetching GitHub followers`);
     const res = await fetch(apiURL);
     const data = await res.json();
     const gtFollower = data.followers;
 
+    spinner.succeed(`Got 'em!!!`);
     console.log(`GitHub Followers ${gtFollower} `);
   } catch (error) {
     handleError(`API CALL FAILED`, error, false, false);
